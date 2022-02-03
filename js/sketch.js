31587bf41bc6eba496fe0b6bc6a827a41cb707e1
est5 <ghostrekon5500@ya.ru>
2022-02-03 22:05:24 +0300

    grid ok hover ok

--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,27 @@
-console.log('numb');
+const color = "red";
+const size = 4;
 
-let x = 4;
-let y = 4;
+let percent = 100 / size;
+const width = percent + "%";
+const paddingBottom = percent + "%";
+console.log(percent);
 
-const grid = document.querySelector('#grid');
+const grid = document.querySelector("#grid");
 
-for (let i = 0; i < x; i++) {
-    for (let j = 0; j < y; j++) {
-        let div = document.createElement('div');
-        div.className = 'section';
-        div.textContent = i + 1;
-        grid.appendChild(div);
-    }
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    let div = document.createElement("div");
+    div.className = "section";
+    div.style.height = 0;
+    div.style.width = width;
+    div.style.paddingBottom = paddingBottom;
+    grid.appendChild(div);
+  }
 }
+
+const section = document.querySelectorAll(".section");
+section.forEach((s) => {
+  s.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = color;
+  });
+});
