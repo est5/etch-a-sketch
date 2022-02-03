a00942624d1610f374dd8d5b2f20abfa9e43209b
est5 <ghostrekon5500@ya.ru>
2022-02-03 22:13:31 +0300

    clear button

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,5 @@
-const color = "red";
-const size = 4;
+const color = "orangered";
+let size = 8;
 
 let percent = 100 / size;
 const width = percent + "%";
@@ -8,16 +8,20 @@ console.log(percent);
 
 const grid = document.querySelector("#grid");
 
-for (let i = 0; i < size; i++) {
-  for (let j = 0; j < size; j++) {
-    let div = document.createElement("div");
-    div.className = "section";
-    div.style.height = 0;
-    div.style.width = width;
-    div.style.paddingBottom = paddingBottom;
-    grid.appendChild(div);
+const init = () => {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      let div = document.createElement("div");
+      div.className = "section";
+      div.style.height = 0;
+      div.style.width = width;
+      div.style.paddingBottom = paddingBottom;
+      grid.appendChild(div);
+    }
   }
-}
+};
+
+init();
 
 const section = document.querySelectorAll(".section");
 section.forEach((s) => {
@@ -25,3 +29,12 @@ section.forEach((s) => {
     e.target.style.backgroundColor = color;
   });
 });
+
+const button = document.querySelector(".button-55");
+button.addEventListener("click", clear);
+
+function clear() {
+  section.forEach((s) => {
+    s.style.backgroundColor = "white";
+  });
+}
